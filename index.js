180af9e153ba9909a62a449f30b5cb6871939d0c
Aiken Tine Ahac <ahac.aiken@gmail.com>
2021-05-23 18:08:24 +0200

    create chat

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const InitiateMongoServer = require("./config/db");
+
+const socketio = require('socket.io')();
+
+// PORT
+const PORT = process.env.PORT || 3001;
+
+// Initiate Mongo Servers
+// InitiateMongoServer().then(r => console.log("Mongo initiated"));
+
+socketio.on("connection", (userSocket) => {
+	console.log("A user connected.")
+
+	userSocket.on("disconnect", (data) => {
+		console.log(data);
+		userSocket.broadcast.emit("receive_message", data.user)
+	})
+	userSocket.on("send_message", (data) => {
+		console.log(data);
+		userSocket.broadcast.emit("receive_message", data)
+	})
+})
+
+socketio.listen(PORT).then(() => {
+	console.log(`Socket open @ port ${PORT}`);
+});
