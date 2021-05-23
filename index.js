40640cbc9036a1278259b37655014e2ce5253c54
Aiken Tine Ahac <ahac.aiken@gmail.com>
2021-05-23 20:23:38 +0200

    pls work

--- index.js
+++ index.js
@@ -4,48 +4,66 @@ dotenv.config();
 // const InitiateMongoServer = require("./config/db");
 
 // PORT
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 // Initiate Mongo Servers
 // InitiateMongoServer().then(r => console.log("Mongo initiated"));
 
-const server = require('http').createServer()
-const io = require('socket.io')(server)
+const WebSocket = require('ws')
+const express = require('express')
+const moment = require('moment')
+const app = express()
 
-io.on('connection', function (client) {
+app.get('/', (req, res) => {
+	res.send("Hello World");
+});
 
-	console.log('client connect...', client.id);
+let  webSockets = {}
 
-	client.on('typing', function name(data) {
-		console.log(data);
-		io.emit('typing', data)
+const wss = new WebSocket.Server({ port })
+wss.on('connection', function (ws, req)  {
+	let userID = req.url.substr(1)
+	webSockets[userID] = ws
+
+	console.log('User ' + userID + ' Connected ')
+
+	ws.on('message', message => { //if there is any message
+		console.log(message);
+		let datastring = message.toString();
+		if(datastring.charAt(0) === "{"){
+			datastring = datastring.replace(/\'/g, '"');
+			let data = JSON.parse(datastring)
+			if(data.auth === "chatapphdfgjd34534hjdfk"){
+				if(data.cmd === 'send'){
+					let boardws = webSockets[data.userid] //check if there is reciever connection
+					if (boardws){
+						let cdata = "{'cmd':'" + data.cmd + "','userid':'"+data.userid+"', 'msgtext':'"+data.msgtext+"'}";
+						boardws.send(cdata); //send message to reciever
+						ws.send(data.cmd + ":success");
+					} else{
+						console.log("No receiver user found.");
+						ws.send(data.cmd + ":error");
+					}
+				} else{
+					console.log("No send command");
+					ws.send(data.cmd + ":error");
+				}
+			} else{
+				console.log("App Authentication error");
+				ws.send(data.cmd + ":error");
+			}
+		} else{
+			console.log("Non JSON type data");
+			ws.send(data.cmd + ":error");
+		}
 	})
 
-	client.on('message', function name(data) {
-		console.log(data);
-		io.emit('message', data)
+	ws.on('close', function () {
+		let userID = req.url.substr(1)
+		delete webSockets[userID]
+		console.log('User Disconnected: ' + userID)
 	})
 
-	client.on('location', function name(data) {
-		console.log(data);
-		io.emit('location', data);
-	})
-
-	client.on('connect', function () {
-	})
-
-	client.on('disconnect', function () {
-		console.log('client disconnect...', client.id)
-		// handleDisconnect()
-	})
-
-	client.on('error', function (err) {
-		console.log('received error from client:', client.id)
-		console.log(err)
-	})
+	ws.send('connected');
 })
 
-server.listen(PORT, function (err) {
-	if (err) throw err
-	console.log('Listening on port %d', PORT);
-});
