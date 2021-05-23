279f72df9668a5bbf86d9f8a874f538f84382418
Aiken Tine Ahac <ahac.aiken@gmail.com>
2021-05-23 19:30:36 +0200

    pls work

--- index.js
+++ index.js
@@ -6,23 +6,46 @@ dotenv.config();
 // PORT
 const PORT = process.env.PORT || 3001;
 
-const app = require('express')()
-const http = require('http').createServer(app)
-
-//Socket Logic
-const socketio = require('socket.io')(http)
-
 // Initiate Mongo Servers
 // InitiateMongoServer().then(r => console.log("Mongo initiated"));
 
-app.get('/', (req, res) => {
-	res.send("Chat server running")
-})
+const server = require('http').createServer()
+const io = require('socket.io')(server)
 
-socketio.on("connection", (userSocket) => {
-	userSocket.on("send_message", (data) => {
-		userSocket.broadcast.emit("receive_message", data)
+io.on('connection', function (client) {
+
+	console.log('client connect...', client.id);
+
+	client.on('typing', function name(data) {
+		console.log(data);
+		io.emit('typing', data)
+	})
+
+	client.on('message', function name(data) {
+		console.log(data);
+		io.emit('message', data)
+	})
+
+	client.on('location', function name(data) {
+		console.log(data);
+		io.emit('location', data);
+	})
+
+	client.on('connect', function () {
+	})
+
+	client.on('disconnect', function () {
+		console.log('client disconnect...', client.id)
+		// handleDisconnect()
+	})
+
+	client.on('error', function (err) {
+		console.log('received error from client:', client.id)
+		console.log(err)
 	})
 })
 
-http.listen(PORT);
+server.listen(PORT, function (err) {
+	if (err) throw err
+	console.log('Listening on port %d', PORT);
+});
