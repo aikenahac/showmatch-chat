8db4956cabc7471b1d78935fce99bf5566fbdd24
Aiken Tine Ahac <ahac.aiken@gmail.com>
2021-05-24 16:27:26 +0200

    fix shit

--- index.js
+++ index.js
@@ -5,25 +5,17 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 
+
 app.get('/', (req, res) => {
-	res.send("ShowMatch chat up!")
+	res.send("Node Server is running. Yay!!")
 })
 
 //Socket Logic
 const socketio = require('socket.io')(http)
 
-socketio.on("connection", (socket) => {
-	console.log("Someone connected to socket!");
-	socket.broadcast.emit("receive_message", "User connected!");
-
-	socket.on("send_message", (data) => {
-		console.log(`Message: ${data}`);
-		socket.broadcast.emit("receive_message", data)
-	})
-
-	socket.on("disconnect", () => {
-		console.log("User disconnected.")
-		socket.broadcast.emit("receive_message", "User disconnected!");
+socketio.on("connection", (userSocket) => {
+	userSocket.on("send_message", (data) => {
+		userSocket.broadcast.emit("receive_message", data)
 	})
 })
 
